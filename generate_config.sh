c635bab2ef219dbc2b6434bff914c7fe9e2f050f
Joel Larsson <tilljoel@gmail.com>
2013-11-12 23:23:35 +0100

    Add: common pidfile in /data

--- generate_config.sh
+++ generate_config.sh
@@ -6,5 +6,6 @@ sed -i "s/^logfile \/var\/log\/redis\/redis-server.log/logfile \/data\/redis-ser
 sed -i "s/^dir \/var\/lib\/redis/dir \/data/g" /data/redis.conf
 sed -i "s/^daemonize yes/daemonize no/g" /data/redis.conf
 sed -i "s/^bind 127.0.0.1/# bind 127.0.0.1/g" /data/redis.conf
+sed -i "s/^pidfile .*pid$/pidfile \/data\/redis-server.pid/g" /data/redis.conf
 
 echo 'generate redis config - done'
